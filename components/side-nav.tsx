61d0b267aeacf8dda1ec5ae656737de2c1d90c8e
crenacernegro <crenacernegro@gmail.com>
2023-12-18 11:30:50 -0500

    added settings page

--- components/side-nav.tsx
+++ components/side-nav.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from "react";
-
 import Link from "next/link";
 import { usePathname, useParams } from "next/navigation";
 import { SideNavItem } from "@/types";
