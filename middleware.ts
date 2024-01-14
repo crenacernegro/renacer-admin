ee24da45b155ec6e3904a8073c9014a12f68d6b3
crenacernegro <crenacernegro@gmail.com>
2024-01-14 15:03:49 -0500

    changed middleware

--- middleware.ts
+++ middleware.ts
@@ -8,6 +8,7 @@ export default authMiddleware({
     "/",
     "/projects",
     "/api/309c0e25-251c-4f13-a300-169ae2a631dd/projects",
+    "/api/309c0e25-251c-4f13-a300-169ae2a631dd/projects/${projectId}",
   ],
 });
 
