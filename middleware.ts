2f49645f8e75855de30587281bfa0bb5a9e12923
crenacernegro <crenacernegro@gmail.com>
2024-01-14 14:57:36 -0500

    changed middleware again

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,11 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/projects"],
+  publicRoutes: [
+    "/",
+    "/projects",
+    "/api/309c0e25-251c-4f13-a300-169ae2a631dd/projects",
+  ],
 });
 
 export const config = {
