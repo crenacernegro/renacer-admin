0f772eb57e6c27535d0a8a0f02b752a1b8e5fecd
crenacernegro <crenacernegro@gmail.com>
2024-01-18 17:01:46 -0500

    chnaged middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/:path*", "/", "/project/:id"],
+  publicRoutes: ["/api/:path*", "/", "/project/:id", "/:storeId"],
 });
 
 export const config = {
